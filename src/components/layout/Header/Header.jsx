ed24144a8c2174dd401b45101581c345d97a6be8
Hoang0504 <ngochoanghuy0504@gmail.com>
2025-11-06 16:06:16 +0700

    update

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <header className="navbar__header">
       <nav className="navbar__container">
         {/* Phần liên kết bên trái */}
-        <ul className="navbar__links-left">
+        <ul className="navbar__links-left navbar__balance">
           <li>
             <Link to="/login" className="navbar__link">
               MEMBERSHIP
@@ -19,7 +19,7 @@ function Header() {
         <a href="https://new-goabove.webflow.io/" className="navbar__logo">
           <img src="/images/logo.svg" loading="lazy" alt="logo" />
         </a>
-        <div></div>
+        <div className="navbar__balance"></div>
         {/* Nút hành động bên phải */}
         {/* <Link to="/payment" className="navbar__button-right">
           BUY NOW
